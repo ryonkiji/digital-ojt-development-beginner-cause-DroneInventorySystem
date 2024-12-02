d6f29f91edb8caabf585d8e25ea390af2e68f069
ryonkiji <ryosuke.kijima96@gmail.com>
2024-12-02 16:10:08 +0900

    feature operationLog

--- DroneInventorySystem/src/main/java/com/digitalojt/web/consts/UrlConsts.java
+++ DroneInventorySystem/src/main/java/com/digitalojt/web/consts/UrlConsts.java
@@ -26,6 +26,12 @@ public class UrlConsts {
 	// 在庫センター情報画面 検索
 	public static final String CENTER_INFO_SEARCH = "/admin/centerInfo/search";
 	
+	// 操作履歴画面
+	public static final String  OPERATION_LOG = "/admin/operationLog";
+	
+	// 操作履歴画面 検索
+	public static final String  OPERATION_LOG_SEARCH = "/admin/operationLog/search";
+	
 	// 認証不要画面
 	public static final String[] NO_AUTHENTICATION = {LOGIN, AUTHENTICATE};
 }
